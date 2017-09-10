119da35c1b4ecbca2fb4a0192f3e19f51bd39c28
Matt Cappleman <matt.cappleman@gmail.com>
2017-09-10 14:00:18 -0400

    Finished Polishing

--- public/static/js/edit.js
+++ public/static/js/edit.js
@@ -10,6 +10,43 @@ if (!accessToken) {
 
 verify();
 
+// tinyMCE.init({
+// 	selector: 'textarea',
+// 	menubar: false,
+// 	height: 500,
+// 	plugins: [
+// 		'advlist autolink lists link image charmap print preview anchor textcolor',
+// 	    'searchreplace visualblocks code fullscreen',
+// 	    'insertdatetime media table contextmenu paste code help'
+// 	],
+// 	toolbar: 'insert | undo redo |  styleselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
+// 	content_css: [
+// 	    '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+// 	    '//www.tinymce.com/css/codepen.min.css'
+// 	]
+// });
+tinymce.init({
+	selector: 'textarea',
+	height: 500,
+	theme: 'modern',
+	plugins: [
+		'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+		'searchreplace wordcount visualblocks visualchars code fullscreen',
+		'insertdatetime media nonbreaking save table contextmenu directionality',
+		'template paste textcolor colorpicker textpattern imagetools codesample toc help emoticons hr'
+	],
+	toolbar1: 'formatselect | bold italic  strikethrough  forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
+	image_advtab: true,
+	templates: [
+		{ title: 'Test template 1', content: 'Test 1' },
+		{ title: 'Test template 2', content: 'Test 2' }
+	],
+	content_css: [
+		'//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+		'//www.tinymce.com/css/codepen.min.css'
+	]
+ });
+
 function submitWiki() {
 
 	var html = tinyMCE.get('editor').getContent()
